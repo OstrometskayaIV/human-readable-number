d504273160e32613c428406d861ae2a873753e0f
Irina Ostrometskaya <i.ostrometskaya@gmail.com>
2022-08-02 14:53:05 +0400

    feat: find the solution for all 1000 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+  let unitsArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let tensArr = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let tempNum = number % 100;
+  let tenth = Math.trunc(tempNum / 10);
+  let units = number % 10;
+  let hundreds = Math.trunc(number / 100);
+  let result = '';
+
+  if (number === 0) {
+    result = 'zero';
+  }
+  else if (tempNum < 20) {
+    result = unitsArr[tempNum];
+  }
+  else {
+    result = tensArr[tenth] + ' ' + unitsArr[units];
+  }
+
+  if (number >= 100 && number <= 999) {
+    result = unitsArr[hundreds] + ' ' + 'hundred' + ' ' + result;
+  }
+  return result.trim();
 }
+
